669b3e1baa21e67c72142a87f570a5c8d18058c3
Adrien <adrien@echoeshq.com>
2025-01-23 10:33:18 +0100

    chore: update tests

--- tests/groups_labels.sh
+++ tests/groups_labels.sh
@@ -6,7 +6,7 @@
 test_should_return_labels() {
     response=$(doRequest "GET" "${PROXY_BASE_PATH}/groups/${GROUP_ID}/labels")
 
-    doHave=$(has "${response}" "id" "26133076")
+    doHave=$(has "${response}" "id" "26982298")
     assert_equals "${doHave}" true
 
     # There are more than 20 labels on the group.
